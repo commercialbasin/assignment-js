0192a6ff04768ebd6199b8540a1a1577c4cd339f
commercialbasin <92856354+commercialbasin@users.noreply.github.com>
2021-11-25 13:46:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const colours = ["green", "red", "rgba(133,122,200)","#f15025"];
+const btn = document.getElementById("btn");
+const colour = document.querySelector(".colour");
+
+ btn.addEventListener("click", function () {
+ // get random number between 0-3 colors[3]
+const randomNumber = getRandomNumber();
+console.log(randomNumber);
+
+document.body.style.backgroundColor = colours[randomNumber];
+colours.textContent = colours [randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colours.length);
+}
